cfad2cc2b53776a7907a83c81d8efb992e4c6bac
Laura Veerkamp <laveerka@f0r5s15.codam.nl>
2025-11-02 10:38:50 +0100

    gnl function error checking

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: laveerka <laveerka@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/10/19 20:06:50 by laveerka      #+#    #+#                 */
-/*   Updated: 2025/11/02 09:43:55 by laveerka      ########   odam.nl         */
+/*   Updated: 2025/11/02 10:30:47 by laveerka      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,14 @@
 char	*get_next_line(int fd)
 {
 	static char	*stash;
+	char		*line;
 
-
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	stash = read_line(stash, fd);
+	line = extract_line(stash, fd);
+	stash = next_line(stash);
+	return (line);
 }
 
 int	main(int argc, char **argv)
